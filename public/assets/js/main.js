e14fcd72d9ef4afff8a67c5642799b40d01e0866
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-04-30 12:16:34 +0200

    json stringify and parse

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,104 +6,118 @@ const buttonEl = document.querySelector('.button');
 const inputEl = document. querySelector('.input');
 const listEl = document.querySelector('.list');
 const listFavEl = document.querySelector('.list_fav');
+//CON LA LINEA SS EL BUTTON SEARCH NO BUSCA
+// const localFavEl = JSON.parse (localStorage.getItem ('favorite')) || []; //Creo la constante para recoger la lista de favoritos y le digo que si tiene guardado en el local storage el item 'favorite' me coja ese y si no tiene nada, que sea un array vacio.
 
 
 // CALL TO THE API
 
 function showpPrintTheData(){
-  event.preventDefault();
+  // event.preventDefault();
   let userinput = inputEl.value;
   
   fetch(`http://api.tvmaze.com/search/shows?q=${userinput}`)
     .then(function(response){
       return response.json();
     })
-    .then(function(data){
-      // console.log(data);
-      // console.log('Esta de arriba es la respuesta de data');
-
+    .then(function(data){ //todo lo que haga lo tengo que hacer dentro de la funcion en el ambito de data, porque todo el tiempo estoy trabajando con la respuesta que me devuelve data que es un array de objetos. Si lo pongo debajo y uso sus datos, me va a decir que no existe.
       // PRINT THE TITLES AND IMAGES     
 
-      listEl.innerHTML = '';
-      listFavEl.innerHTML = '';
-      for (let i = 0; i < data.length; i++){
-        // console.log(data[i].show.name);
-        // console.log(data[i].show.image.medium);
+      listEl.innerHTML = ''; //con la lista vacia le digo que con cada busqueda la lista ul se me quede vacia antes de cada nueva busqueda.
 
+      // listFavEl.innerHTML = ''; //No hace falta, porque quiero que se me almacenen constantemente en favoritos, no quiero vaciar el array.
+
+      for (let i = 0; i < data.length; i++){
+        console.log(data);
         const showImageMedium = data[i].show.image.medium;
+        console.log (showImageMedium);
         const showName = data[i].show.name;
         const showId = data[i].show.id;
         // console.log(Showid);
 
         const newLi = document.createElement('li');
+        newLi.setAttribute ('class', 'item_list');
+        newLi.setAttribute ('id', showId);
+        // const contentId = document.createTextNode (showId);
+
         const newTitle = document.createElement ('h3');
-        const newImg = document.createElement ('img');
-
-        listEl.appendChild (newLi);
-        newLi.appendChild (newTitle);
-        newLi.appendChild (newImg);
-
         const contentnewTitle = document.createTextNode (showName);
-        const contentnewImg = document.createTextNode (showImageMedium);
-        const contentId = document.createTextNode (showId);
 
-        newTitle.appendChild(contentnewTitle);
-        newImg.appendChild(contentnewImg);
-        newLi.appendChild(contentId);
+        const newImg = document.createElement ('img');
+        // const contentnewImg = document.createTextNode (showImageMedium);
 
         newImg.setAttribute('src', showImageMedium);
         if (showImageMedium === null){
           newImg.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
         }
+       
+        listEl.appendChild (newLi);
+        newLi.appendChild (newTitle);
+        newLi.appendChild (newImg);
+
+        newTitle.appendChild(contentnewTitle);
       }
-      listEl.innerHTML = listEl.innerHTML; selectTheFavourite (); //The list gets filled and the function that listens to the series clicked is executed
+
+      //Sigo dentro de data pero no dentro del bucle
+
+      const resultList = document.querySelectorAll('.item_list'); //me devuele un array de objetos donde cada objeto es el li, uno por cada resultado
+      console.log (resultList);
+
+      for (let index = 0; index < resultList.length; index++){
+        resultList[index].addEventListener ('click', handlerFavorite);
+      }
+
+      // ADD FAVOURITE
+      function handlerFavorite (event) {
+        const clicked = event.currentTarget;
+        console.log (clicked); //console.log me da solo el elemento pero...console.dir me da el mismo elemento pero como objeto y esto si que lo puedo utilizar, para buscar los datos que necesito y acceder a ellos a traves de las claves del objeto.
+        console.dir (clicked);
+        clicked.classList.toggle('favorite'); //1 paso completado, nos pone la clase favorito si click
+
+        const showImageFav = clicked.lastChild.src;
+        console.log(showImageFav);
+
+        const showNameFav = clicked.outerText;
+        console.log (showNameFav);
+
+        const showIdFav = clicked.id;
+
+        if (clicked.classList.contains('favorite')){
+          const itemFav = document.createElement ('li');
+          const imgFav = document.createElement ('img');
+          imgFav.setAttribute('src', showImageFav);
+          if (showImageFav === null){
+            imgFav.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
+          }
+          const titleFav = document.createElement ('h4');
+          const contentNewTitleFav = document.createTextNode (showNameFav);
+        
+          listFavEl.appendChild(itemFav);
+          itemFav.appendChild(imgFav);
+          itemFav.appendChild(titleFav);
+          titleFav.appendChild(contentNewTitleFav);
+        }
+        
+        // SAVE IN LOCAL STORAGE
+        //Creo un objeto con los datos que he sacado
+
+        const localObject = {
+          'name': showNameFav,
+          'image': showImageFav,
+          'id': showIdFav,
+        };
+        console.log(localObject);
+
+        localFavEl.push(localObject); //Al array vacio le añado cada serie como objeto
+
+        localStorage.setItem ('favorite', JSON.stringify(localFavEl));// Guardo el array de objetos con el nombre 'favorite' con stringify para convertirlo en  cadena, ya que en el local storage no se pude guardar un array, asi que con strinfigy lo guardo en cadena.
+
+      }
     });
 }
 
 buttonEl.addEventListener('click', showpPrintTheData);
 
-// SAVE IN LOCAL STORAGE
-// ADD FAVOURITE
-
-function convertToFavStoreInCache(event){ //Listens if serie clicked (currentTarget) and adds class favourite.  Later it storages it into the cache
-  const serieclicked = event.currentTarget;
-  serieclicked.classList.toggle('favourite');
-
-  const idSeries = document.querySelectorAll ('item_li'); //Saves all the items of the list in an array.
-
-  // console.log (idSeries);
-
-  let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []; //The variable itemsArray gets the items of the list that are in an array. These items were saved in the localStorage two lines below, with the localStorage.setItem. The condition is that if the items are storaged in the browser cache, they should be 
-
-  itemsArray.push(idSeries);//The push method includes in the array created in the local storage the arrray with the id of series that was created before with qsAlll in const idSeries.
-
-  localStorage.setItem('items', JSON.stringify(itemsArray)); //Gets the array of items, converts it into string and saves it in the local storage as 'items'.
-  // }
-
-  // function takeStorageFavAndPrint () {
-
-  console.log(listFavEl);
-  
-  const ItemsFav = document.querySelectorAll('.favourite');
-  // listFavSeries.innerHTML += ItemsFavSeries;
-  
-  // for (let i=0; i < ItemsFav; i++){
-
-  listFavEl.innerHTML += listFavEl.innerHTML + (`<li class='fav_li'> <h4>${serieclicked.firstElementChild.firstChild.data}</h4> <img src= '${serieclicked.firstChild.nextElementSibling.style.backgroundImage}</li>`);
-
-  console.log(ItemsFav);
-  console.log(serieclicked);
-}
-
-
-function selectTheFavourite() { // Function that selects only the items of series that have been clicked. It saves the items into an array and goes through them with a loop that executes in each of them and Eventlistener if the item has been clicked and excutes on that concrete item the function that adds to fav and stores in cache.
-  const showItems = document.querySelectorAll('.item_li'); //storages the series into an array (declared locally, just in this function, not globally)
-  for ( const item of showItems) { // loop that goes through the array listening to which serie has been clicked and if clicked it executes the function that adds the class favourite and stores them into the browser cache
-  // console.log(item);
-  // console.log (showItems);
-    item.addEventListener('click', convertToFavStoreInCache);
-  }
-}
 
 
 
